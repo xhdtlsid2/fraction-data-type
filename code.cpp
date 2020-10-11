f55311a190df7c93360ba511532ca0bd65119b26
xhdtlsid2 <48355679+xhdtlsid2@users.noreply.github.com>
2020-10-11 22:44:18 +0900

    Update code.cpp
    added unary minus operator, absolute value function

--- code.cpp
+++ code.cpp
@@ -14,30 +14,19 @@ struct fraction{
 
 	fraction(ll _a = 0) : a(abs(_a)), b(1), neg(_a < 0) {}
 	fraction(ll _a, ll _b) : a(abs(_a)), b(abs(_b)), neg((_a < 0 && _b > 0) || (_a > 0 && _b < 0)) {
-		if(!b) {
-			cout << "0";
-			throw "cannot divide by 0";
-		}
+		if(!b) throw "cannot divide by 0";
 		ll k = mygcd(a, b);
 		a /= k, b /= k;
 	}
 	fraction(ll _a, ll _b, bool _neg) : a(abs(_a)), b(abs(_b)), neg(_neg) {
-		if(!b) {
-			cout << "0";
-			throw "cannot divide by 0";
-		}
-		if(_a < 0 || _b < 0 || (!_a && _neg)) {
-			if(_a < 0)cout << "A";
-			else if( _b < 0)cout << "B";
-			else cout << "NEG";
-			cout << "\ninit\n";
-			throw "invalid initialization";
-		}
+		if(!b) throw "cannot divide by 0";
+		if(_a < 0 || _b < 0 || (!_a && _neg)) throw "invalid initialization";
 		ll k = mygcd(a, b);
 		a /= k, b /= k;
 	}
 
 	bool operator!() { return !a; }
+	fraction operator-(){ return !(*this) ? (*this) : fraction(a, b, !neg); }
 	fraction operator+(fraction t){
 		ll c = mylcm(b, t.b);
 		if(!neg && !t.neg) return fraction(a * (c / b) + t.a * (c / t.b), c);
@@ -45,27 +34,19 @@ struct fraction{
 		else if(neg && !t.neg) return t + (*this);
 		else return fraction(a * (c / b) + t.a * (c / t.b), c, true);
 	}
-	fraction operator-(fraction t){
-		if(!!t) t.neg = !t.neg;
-		return (*this) + t;
-	}
 	fraction operator*(fraction t){
 		if(!(*this) || !t) return fraction(0);
 		ll c = mygcd(a, t.b), d = mygcd(b, t.a);
 		return fraction((a / c) * (t.a / d), (b / d) * (t.b / c), neg != t.neg);
 	}
-	fraction operator/(fraction t){
-		return (*this) * fraction(t.b, t.a);
-	}
-	fraction operator-(){
-		fraction t(*this);
-		if(!!t) t.neg = !t.neg;
-		return t;
-	}
+	fraction operator-(fraction t){ return (*this) + (-t); }
+	fraction operator/(fraction t){ return (*this) * fraction(t.b, t.a); }
 	bool operator==(fraction t){ return (a == t.a && b == t.b && neg == t.neg); }
 	bool operator!=(fraction t){ return !((*this) == t); }
 	bool operator>=(fraction t){ return ((*this) - t).neg; }
 	bool operator>(fraction t){ return (((*this) != t) && ((*this) >= t)); }
 	bool operator<=(fraction t){ return !((*this) > t); }
 	bool operator<(fraction t){ return !((*this) >= t); }
+	
+	fraction absVal(){ return fraction(a, b, true); }
 };
